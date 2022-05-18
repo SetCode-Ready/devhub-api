52a738b8c416134448ce8bbf52c936ac66beadf1
kauecdev <kaue_cavalcante.cnt@outlook.com>
2022-05-18 18:26:27 -0300

    mod: changed constructor

--- src/core/entities/domain/studant.ts
+++ src/core/entities/domain/studant.ts
@@ -2,11 +2,8 @@ import { ProfessionalInfo } from './professional-info';
 import { UserBase } from '../base/user.base';
 
 export class Studant extends UserBase {
-  constructor(name: string, email: string, password: string) {
+  constructor() {
     super();
-    this.name = name;
-    this.email = email;
-    this.password = password;
   }
 
   professionalInfos: ProfessionalInfo;
