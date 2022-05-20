812c4a993cbdd6d8f053f43700385cf9c671fd89
Emanuelvss13 <emanuelsouza2002@gmail.com>
2022-05-20 16:44:26 -0300

    refactor: create student command with nest dp

--- src/use-cases/command/studant/create-studant-account.command.ts
+++ src/use-cases/command/studant/create-studant-account.command.ts
@@ -1,8 +1,14 @@
-import { StudantRepository } from 'src/core/repositories/studant.repository';
+import { StudantRepositoryMock } from '../../../data/mocks/repositories/studant.repository.mock';
+import { REPOSITORY } from './../../../core/constants/repository.enum';
+import { Inject, Injectable } from '@nestjs/common';
 import { ICreateStudantAccountRequest } from 'src/shared/request-models/create-studant-account.request';
 
+@Injectable()
 export class CreateStudantAccountCommand {
-  constructor(private readonly studantRepository: StudantRepository) {}
+  constructor(
+    @Inject(REPOSITORY.STUDANT_REPOSITORY)
+    private readonly studantRepository: StudantRepositoryMock,
+  ) {}
 
   async execute(input: ICreateStudantAccountRequest) {
     const newStudantAccount = await this.studantRepository.create(input);
