9570a7386f7b4a55010427c9875492e50879d265
Emanuelvss13 <emanuelsouza2002@gmail.com>
2022-05-25 14:02:06 -0300

    feat: teacher repository mock

---
+++ src/data/mocks/repositories/teacher.repository.mock.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { Teacher } from 'src/core/entities/domain/teacher';
+import { ITeacherRepository } from 'src/core/repositories/teacher.repository';
+import { ProfessionalInfo } from '../../../core/entities/domain/professional-info';
+import { ICreateProfessionalInfoDTO } from '../../../shared/dto/create-professional-info.dto';
+
+@Injectable()
+export class TeacherRepositoryMock implements ITeacherRepository {
+  private teachers = [];
+
+  createProfessionalInfo(
+    input: ICreateProfessionalInfoDTO,
+  ): Promise<ProfessionalInfo> {
+    throw new Error('Method not implemented.');
+  }
+  findByRegistry(registry: string): Promise<Teacher> {
+    throw new Error('Method not implemented.');
+  }
+  findByEmail(email: string): Promise<Teacher> {
+    throw new Error('Method not implemented.');
+  }
+  public async create(input: any): Promise<Teacher> {
+    const newTeacher = new Teacher();
+
+    Object.assign(newTeacher, { id: 'uuid', ...input });
+
+    this.teachers.push(newTeacher);
+    return newTeacher;
+  }
+  findById(id: string): Promise<Teacher> {
+    throw new Error('Method not implemented.');
+  }
+  async findAll(): Promise<Teacher[]> {
+    return this.teachers;
+  }
+  updateById(id: string): Promise<Teacher> {
+    throw new Error('Method not implemented.');
+  }
+  deleteById(id: string): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+}
