8e3f7105cb0dd06012086ee3504aded9d2f34f3f
Emanuelvss13 <emanuelsouza2002@gmail.com>
2022-05-25 15:56:53 -0300

    feat: update studant use case

--- src/core/repositories/base.repository.ts
+++ src/core/repositories/base.repository.ts
@@ -2,6 +2,6 @@ export interface BaseRepository<T> {
   create(input: any): Promise<T>;
   findById(id: string): Promise<T>;
   findAll(): Promise<T[]>;
-  updateById(id: string): Promise<T>;
-  deleteById(id: string): Promise<boolean>;
+  updateById(id: string, data: any): Promise<T>;
+  deleteById(id: string, data: any): Promise<boolean>;
 }
