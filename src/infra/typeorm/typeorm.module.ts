921b6b4d64556a6deae23635172d4cde44d95816
kauecdev <kaue_cavalcante.cnt@outlook.com>
2022-05-25 16:22:12 -0300

    feat: changed implementation to use env variables

--- src/infra/typeorm/typeorm.module.ts
+++ src/infra/typeorm/typeorm.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'devhub',
-      entities: [],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 15432,
+        username: configService.get('POSTGRES_USERNAME'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DATABASE'),
+        entities: [],
+        synchronize: true,
+      }),
     }),
   ],
 })
